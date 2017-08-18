7f1a04802b93d47492f7e115967d00bec59cfe2f
oscar <oscarcased2017@gmail.com>
2017-08-18 13:02:46 -0500

    cierre de la seccion10

--- src/app/components/data/data.component.ts
+++ src/app/components/data/data.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl,  Validators, FormArray } from '@angular/forms';
+import { Observable } from 'rxjs/Rx';
 
 
 @Component({
@@ -41,9 +42,17 @@ export class DataComponent implements OnInit {
             new FormControl("correr", Validators.required),
             new FormControl("dormir", Validators.required),
             new FormControl("comer", Validators.required)
-        ])
+        ]),
+        username: new FormControl("", [Validators.required, Validators.minLength(3)], this.existeUsuario),
+        pass1: new FormControl("", [Validators.required, Validators.minLength(3)]),
+        pass2: new FormControl()
   										
   		});
+
+       this.forma.controls['pass2'].setValidators([
+         Validators.required, 
+         this.noIgual.bind(this.forma)  //esto equivale a decir que dentro de la funcion noIgual "this" va a ser "this.forma"
+       ])
 
   		//this.forma.setValue(this.usuario); //esto me ahora tener que inicializar cada valor
 
@@ -103,10 +112,37 @@ export class DataComponent implements OnInit {
 		*/  
 
 
+  //esto retorna un observador(que esta pendiente de la data), por tanto me suscribo
+   /*este se dispara por cualquier cambio del formulario(en cualquier campo)
+   this.forma.valueChanges 
+     .subscribe( data=>{
+         console.log(data);
+     });
+     */
+     //para un campo especifico
+
+
+     //valores del campo
+   this.forma.controls['username'].valueChanges 
+     .subscribe( data=>{
+         console.log(data);
+     });
+
+
+     //status del campo
+   this.forma.controls['username'].statusChanges
+     .subscribe( data=>{
+         console.log(data);
+     });
+
+
+
+
   }
 
   ngOnInit() {
   }
+
 
   agregarPasatiempo() {
       //castear a un array de elemento
@@ -124,6 +160,8 @@ export class DataComponent implements OnInit {
        */
 
   }
+
+
 
 guardarCambios(){
 	console.log('posteando');
@@ -175,6 +213,61 @@ noHerrera( control:FormControl):{ [s:string]:boolean } {
 
 }
 
+
+noIgual ( control:FormControl): { [s:string]:boolean } {   //lo correcto es { [s:string]:boolean }    pero puedes usar "any"
+   //como lo voy a poner en el pass2 entonces control.value=pass2
+
+   
+   /* 
+   como nuestras validaciones estan en un contexto aparte, pues no reconoce al objeto "this"
+   en el momento que esta corriendo esta funcion estará corriendo en otro contexto donde  "this" no existe
+   para esto debemos especificar bind
+
+      this.noIgual.bind(this.forma)  //esto equivale a decir que dentro de esta funcion noIgual "this" va a ser "this.forma"
+  */
+
+    let miforma:any = this;  //en este caso como fue pasado por bind this ahora es "this.forma"
+   
+   if (control.value !== miforma.controls["pass1"].value) { //validacion fallo //this.forma.get("pass1").value
+     return { 
+       esIgual:true
+     }
+   } else { ////validacion exitosa
+       return null;
+   }
+   
+
+   //console.log(this);
+
+}
+
+
+
+/*Validacion personalizada asyncrona. Este me va a retornar una promesa o un observable(para el observable necesitamos import { Observable } from 'rxjs/Rx';).
+*/
+
+existeUsuario ( control:FormControl): Promise<any> | Observable<any> {   
+
+    //necesitamos hacer un proceso asyncrono que retorne "null si pasa", o un "objeto sino pasa"
+   let promesa = new Promise(
+             (resolve, reject) =>{
+                 setTimeout( ()=>{
+                        if (control.value=="osmel") {
+                             resolve({   //si existe enviamos "existe:true"   
+                                existe:true   
+                             })
+                        } else {  // caso contrario null
+                              resolve(null)
+                        }
+
+                 }, 3000)
+             }
+   )
+   return promesa;
+
+   
+
+}
 
 }
 
